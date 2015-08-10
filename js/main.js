c01dc204526cc7d5a7d7fd8eb97e2719a96ae77b
brandon932 <bthornton_932@yahoo.com>
2015-08-10 10:20:44 -0600

    all loops working

--- js/main.js
+++ js/main.js
@@ -1,3 +1,64 @@
 // add scripts
+var students = [
+  {
+   name: 'Derek',
+   age: 25,
+   city: 'Boulder'
+  },
+  {
+   name: 'Liz',
+   age: 28,
+   city: 'Denver'
+  },
+  {
+   name: 'Carl',
+   age: 31,
+   city: 'Boulder'
+  },
+  {
+   name: 'Peter',
+   age: 23,
+   city: 'Boulder'
+  },
+  {
+   name: 'Megan',
+   age: 19,
+   city: 'Denver'
+  }
+];
 
-console.log("sanity check!");
+var firstLoop = function(arr){
+  arr.forEach(function(student){
+    console.log(student.name);
+  });
+};
+// firstLoop(students);
+var secondLoop = function(arr){
+  arr.forEach(function(student){
+    console.log(student.age);
+  });
+};
+// secondLoop(students);
+var thirdLoop= function(arr){
+  arr.forEach(function(student){
+    console.log(student.name + ", " + student.city);
+  });
+};
+// thirdLoop(students);
+var fourthLoop= function(arr){
+    arr.forEach(function(student){
+     if (student.city === "Boulder"){
+    console.log(student.name+ " is from "+ student.city);
+     }
+  });
+};
+// fourthLoop(students);
+var fithLoop= function(arr){
+  arr.forEach(function(student){
+    if (student.age > 25){
+      console.log( student.name + " is older then 25");
+    }
+  });
+};
+fithLoop(students);
+// TODO: refactor to use more opp and push to github
